c484c04e83ce91e334eeb9a7c84d1bcf5472b493
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-28 15:14:18 -0500

    updated users db & controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token(0,20)
-    end
+ 
+ validates :email, presence: true, uniqueness: true
+ validates :first_name, :last_name, presence: true 
+ has_secure_password
+  def to_s
+    self.first_name + " " + self.last_name
   end
 end
