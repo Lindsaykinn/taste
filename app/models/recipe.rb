a5c2f620200d7b7cd6032dd42b2b63e0dab32ec4
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-28 15:15:15 -0500

    updated routes and recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,21 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :ingredients
+  has_many :directions
+  validates :title, presence: true
+  
+
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.each do |ingredient_attributes|
+      self.ingredients.build(ingredient_attributes)
+    end
+  end
+
+  def instructions_attributes=(instructions_attributes)
+    instructions_attributes.each do |instruction_attributes|
+      self.instructions.build(instruction_attributes)
+    end
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  resources :recipes
-  devise_for :users, :controllers => {registrations:'registrations'}
-
   root to: 'static#home'
+  resources :recipes
+
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy', as: 'destroy_user_session'
+  
+  
+  # match '/auth/:google_oauth2/callback' => 'sessions#google', via: [:get,:post]
+  # match '/auth/:google_oauth2/callback' => 'sessions#omniauth', via: [:get, :post]
+
+  get 'auth/google_oauth2/callback', to: 'sessions#omniauth'
 
   
 end
