47fcc8ebfce62afd082d23bab04721dee07cd785
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-30 16:53:11 -0500

    added new/edit to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,16 @@
 class CategoriesController < ApplicationController
+  before_action :redirect_if_not_logged_in
+  before_action :find_category, only: [:edit, :update, :destroy]
 
   def index
-    @categories = Category.all
+    @categories = Category.all.alphabetize
   end
 
   def new
     @category = Category.new
 
     @category.recipes.build
+
   end
 
   def create
@@ -15,8 +18,9 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to recipes_path
+      redirect_to categories_path
     else
+      flash.now[:error] = @category.errors.full_messages
       render :new
     end
   end
@@ -25,7 +29,30 @@ class CategoriesController < ApplicationController
     @category = Category.find_by_id(params[:id])
   end
 
+  def update
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      flash.now[:error] = @category.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    # if @category
+    @category.destroy
+    flash[:notice] = "#{@category.name} was deleted."
+    redirect_to category_path
+    # elsif @category.recipe
+    #   flash[:notice] = "#{@category.name} is associated with an existing recipe and cannot be deleted."
+    # end
+  end
+
   private
+
+  def find_category
+    @category = Category.find_by_id(params[:id])
+  end
   
   def category_params
     params.require(:category).permit(:name, :user_id, recipes_attributes: [:title, :description, :ingredient, :instructions, :rating])
