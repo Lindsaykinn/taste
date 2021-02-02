c375e50b3d1bf22e7da11b2a5d398e02f057261f
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-02 14:55:47 -0500

    added new/show/create to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  def new 
+    @comment = Comment.new
+  end
+
+  def show
+    comment = Comment.find_by(user: current_user, recipe: find_by_recipe_id)
+    # respond_to do |f|        
+    #   f.json {render json: comment}       
+    # end 
+  end
+
+  def create 
+    if user_signed_in?
+      comment = Comment.new(comment_params)
+      comment.recipe = find_by_recipe_id
+      comment.user = current_user
+      if comment.save
+        redirect_to recipe_path
+      end
+    else
+      flash[:notice] = "You must be logged in to post a comment."
+    end
+  end
+
 end
