5b44c9ff17f0e917e3f69e90990028bbc5d5b9ec
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-02 17:07:40 -0500

    changed users relationships order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
- has_many :recipes, through: :categories
- has_many :categories
+  has_many :categories
+  has_many :recipes, through: :categories
  has_secure_password
  validates :email, presence: true, uniqueness: true
  validates :first_name, :last_name, presence: true 
