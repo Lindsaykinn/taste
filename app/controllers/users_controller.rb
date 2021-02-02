9e9819022d89810aae388f30db8f048907ce9a43
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-02 14:41:37 -0500

    added styling & class for fieldswitherrors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,22 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to recipes_path 
     else
+      flash.now[:notice] = "Email provided already has an account"
       render :new
     end
+  end
+
+  def show
+    find_user
+    render :show
   end
 
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
+    def find_user
+      @user = User.find_by_id(params[:id])
     end
 end
