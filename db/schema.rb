5b3992d0134c6998600a29810428aac52f29cd16
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-05-10 15:01:16 -0400

    added postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_193853) do
+ActiveRecord::Schema.define(version: 2021_02_07_174204) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_02_02_193853) do
   create_table "instructions", force: :cascade do |t|
     t.string "step"
     t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.string "measurement"
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
